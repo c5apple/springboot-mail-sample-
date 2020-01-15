290b237d825f0fd6bc09abe67a39fa82bd8d434a
c5apple <c5apple_party@yahoo.co.jp>
2020-01-15 13:57:20 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SpringbootMailSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMailSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMailSampleApplication.class, args);
+	}
+
+}
