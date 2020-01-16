72c5e14b2c6a7730c39f547249eca39c3c6af594
c5apple <c5apple_party@yahoo.co.jp>
2020-01-16 17:25:22 +0900

    HTMLメール送信処理

--- src/main/java/com/example/demo/SpringbootMailSampleApplication.java
+++ src/main/java/com/example/demo/SpringbootMailSampleApplication.java
@@ -1,11 +1,24 @@
 package com.example.demo;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class SpringbootMailSampleApplication {
@@ -15,7 +28,7 @@ public class SpringbootMailSampleApplication {
 
 		try (ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootMailSampleApplication.class, args)) {
 			SpringbootMailSampleApplication app = ctx.getBean(SpringbootMailSampleApplication.class);
-			app.sendMail();
+			app.sendMail2();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -23,6 +36,9 @@ public class SpringbootMailSampleApplication {
 
 	@Autowired
 	private MailSender mailSender;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	/**
 	 * メールを送信する
@@ -38,6 +54,46 @@ public class SpringbootMailSampleApplication {
 		msg.setText("Spring Boot より本文送信");
 
 		mailSender.send(msg);
+
+		System.out.println("End!!");
+	}
+
+	/**
+	 * メールを送信する
+	 */
+	public void sendMail2() {
+		System.out.println("Start!!");
+
+		// テストデータ
+		Context context = new Context();
+		context.setVariable("name", "ユーザー名");
+		List<String> itemList = Arrays.asList("1", "2");
+		context.setVariable("itemList", itemList);
+
+		MimeMessagePreparator message = new MimeMessagePreparator() {
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,
+						StandardCharsets.UTF_8.name());
+				helper.setFrom("送信元メールアドレス"); // FIXME
+				helper.setTo("宛先メールアドレス"); // FIXME
+				helper.setSubject("テストメール");
+
+				String templateName = "samplemail";
+				SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+				ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+				templateResolver.setTemplateMode(TemplateMode.HTML);
+				templateResolver.setPrefix("mailtemplate/");
+				templateResolver.setSuffix(".html");
+				templateResolver.setCharacterEncoding("UTF-8");
+				templateResolver.setCacheable(true);
+				templateEngine.setTemplateResolver(templateResolver);
+				String text = templateEngine.process(templateName, context);
+				helper.setText(text, true);
+			}
+		};
+
+		javaMailSender.send(message);
 
 		System.out.println("End!!");
 	}
